4a4ec4971082779c81ebb507c726bf1570a697d2
MehmetC <mehmet.catikkaya@codeitlabs.com>
2022-08-31 15:40:24 +0300

    remove tabss

--- src/json-viewers/diff/ChipBar.svelte
+++ src/json-viewers/diff/ChipBar.svelte
@@ -2,7 +2,7 @@
   export const title = '';
   import Chip from './Chip.svelte';
   import TypeList from './TypeList.svelte';
-  import { base, compare } from '../../data/data.service';
+  import { base } from '../../data/data.service';
   let baseElement;
   let compareElement;
   let showList = false;
@@ -46,17 +46,6 @@
       d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"
     /></svg
   >
-  <span
-    bind:this={compareElement}
-    on:click={(e) => {
-      option = compare;
-      showList = true;
-      pos = getXY(e);
-      e.stopPropagation();
-    }}
-  >
-    <Chip type="compare" title={$compare} />
-  </span>
 </div>
 {#if showList}
   <TypeList
